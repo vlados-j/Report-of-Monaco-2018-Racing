77c060157519a33aa918990a03adc8ab721cc768
Vladyslav Dzhulai <dzhulai.vladyslav@gmail.com>
2022-08-28 23:23:19 -0400

    Updated a report

--- application/report.py
+++ application/report.py
@@ -1,12 +1,10 @@
 from datetime import datetime
 
 
-"""
-It's a draft for task 6. Here is only several functions of processing data
-"""
-
-
 def collecting_racers_data(file_path: str, title_for_time: str):
+    """
+    Current function returns takes log file, and returns sorted list with racer abbreviation and time of each racer
+    """
     with open(file_path, 'r') as file:
         players_data = []
         players_list = file.read().splitlines()
@@ -19,6 +17,9 @@ def collecting_racers_data(file_path: str, title_for_time: str):
 
 
 def collecting_abbreviations(file_path: str):
+    """
+    Current function takes the path of the file, and returns the name of players, teams, and theirs abbreviations
+    """
     with open(file_path, 'r') as unsorted_file:
         abbreviations_list = []
         abbreviations = unsorted_file.read().splitlines()
@@ -32,12 +33,53 @@ def collecting_abbreviations(file_path: str):
         return abbreviations_list
 
 
+def lap_time_calculator(start_time: str, finish_time: str):
+    """
+    Current function takes start and end time of each player, and calculates the lap time.
+    If the time is negative, function returns an error
+    """
+    try:
+        start_time = datetime.strptime(start_time, '%Y-%m-%d_%H:%M:%S.%f')
+        finish_time = datetime.strptime(finish_time, '%Y-%m-%d_%H:%M:%S.%f')
+        lap_time = str(finish_time - start_time)
+        if lap_time[0] == '-':
+            return 'Incorrect time. Please, check logs.'
+        else:
+            return str(finish_time - start_time)[:-3]
+    except Exception:
+        return 'Unexpected error. Please, check if the logs are correct'
+
+
 def data_merge():
+    """
+    Current function takes all the parsed files, and returns combined a list with info about the racers for output
+    """
     start_data = collecting_racers_data('../files/start.log', 'start_time')
     finish_data = collecting_racers_data('../files/end.log', 'finish_time')
     abbreviations_data = collecting_abbreviations('../files/abbreviations.txt')
+    collected_data = []
+    for racer_index in range(len(start_data)):
+        if start_data[racer_index]['racer_abbreviation'] == finish_data[racer_index]['racer_abbreviation'] == abbreviations_data[racer_index]['racer_abbreviation']:
+            collected_data.append({
+                'full_name': abbreviations_data[racer_index]['full_name'],
+                'team': abbreviations_data[racer_index]['team'],
+                'lap_time': lap_time_calculator(start_data[racer_index]['start_time'], finish_data[racer_index]['finish_time'])
+            })
+        else:
+            collected_data.append({
+                'full_name': 'Invalid data',
+                'team': 'Invalid data',
+                'lap_time': 'Invalid data'
+            })
+    collected_data.sort(key=lambda a: a['lap_time'])
+    return collected_data
 
 
-print(collecting_racers_data('../files/start.log', 'start_time'))
-print(collecting_racers_data('../files/end.log', 'finish_time'))
-print(collecting_abbreviations('../files/abbreviations.txt'))
+def print_report():
+    """
+    Current function takes all sorted data, and returns structured data to a console
+    """
+    pass
+
+
+print(data_merge())
