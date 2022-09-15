e20ab5d2b9baefbb92c69f5eb4ba012a87e8441f
Vladyslav Dzhulai <dzhulai.vladyslav@gmail.com>
2022-09-15 11:10:50 -0400

    Refactoring

--- application/report.py
+++ application/report.py
@@ -4,10 +4,11 @@ from os import path
 
 
 def get_parser():
-    """It's the parser for a command line"""
+    """The parser for a command line"""
     my_parser = argparse.ArgumentParser(description='Shows the results table of F1 players, or stats of each player')
 
-    my_parser.add_argument('--files', type=str, help='the path to folder with the logs and abbreviations')
+    my_parser.add_argument('--files', type=str, required=True,
+                           help='the path to folder with the logs and abbreviations')
     my_parser.add_argument('--driver', type=str, help='name of the specific driver to output info about him')
     my_parser.add_argument('--asc', action='store_false', help='shows the list of players in ascending order by time')
     my_parser.add_argument('--desc', action='store_true', help='shows the list of players in descending order by time')
@@ -17,71 +18,91 @@ def get_parser():
 
 def cli():
     """
-    Current function parses data from the command line, and execute the final result by calling
-    necessary functions
+    Current function parses the arguments from command line, assign 3 paths (start_path, finish_path,
+    abbreviations_path), and executing either info about the racer, either the list of racers with the results
     """
     parser = get_parser()
     parsers_data = parser.parse_args()
-    if parsers_data.files is not None and path.isdir(parsers_data.files):
-        start_path = parsers_data.files + '/start.log'
-        finish_path = parsers_data.files + '/end.log'
-        abbreviations_path = parsers_data.files + '/abbreviations.txt'
-        try:
-            collected_abbreviations = collecting_abbreviations(abbreviations_path)
-            collected_data = collecting_racers_data(start_path, finish_path, collected_abbreviations)
-            structured_info = players_info(collected_data)
-            if parsers_data.driver is not None and structured_info[parsers_data.driver]['lap_time'] > timedelta(0):
-                print('Racer -', structured_info[parsers_data.driver]['name'],
-                      '\nTeam -', structured_info[parsers_data.driver]['team'],
-                      '\nTime -', str(structured_info[parsers_data.driver]['lap_time'])[3:-3])
-            elif parsers_data.driver is not None:
-                print('Racer -', structured_info[parsers_data.driver]['name'],
-                      '\nTeam -', structured_info[parsers_data.driver]['team'],
-                      '\nTime - Incorrect data. Check logs')
-            else:
-                prepared_info_for_report = build_report(structured_info, parsers_data.desc)
-                print_report(prepared_info_for_report)
-        except FileNotFoundError as e:
-            print(e, 'Please, check if the files start.log, end.log, abbreviations.txt files are exist')
-        except KeyError:
-            print('Probably there is the wrong racers name. Please, check the spelling')
-        except Exception:
-            print('Something went wrong!')
-    else:
-        print('\nThere is no such a directory\n')
-        parser.print_help()
+
+    start_path = path.join(parsers_data.files, 'start.log')
+    finish_path = path.join(parsers_data.files, 'end.log')
+    abbreviations_path = path.join(parsers_data.files, 'abbreviations.txt')
+
+    try:
+        structured_info = processing_data(start_path, finish_path, abbreviations_path)
+        if parsers_data.driver:
+            drivers_info = structured_info[parsers_data.driver]
+            print('Racer -', drivers_info.name,
+                  '\nTeam -', drivers_info.team,
+                  '\nTime -', drivers_info.lap_time_str)
+        else:
+            prepared_info_for_report = build_report(structured_info, parsers_data.desc)
+            print_report(prepared_info_for_report)
+    except FileNotFoundError as e:
+        print(e, 'Please, check the folder path and if the files start.log, end.log, abbreviations.txt files are '
+                 'exist')
+    except KeyError:
+        print('Probably there is the wrong racers name. Please, check the spelling')
+    except Exception:
+        print('Something went wrong!')
+
+
+def processing_data(start_path, finish_path, abbreviations_path):
+    """Function calls the necessary functions to get info for CLI function about all the racers"""
+    collected_abbreviations = collecting_abbreviations(abbreviations_path)
+    collected_data = collecting_racers_data(start_path, finish_path, collected_abbreviations)
+    return players_info(collected_data)
 
 
 def collecting_abbreviations(file_path: str):
     """
-    Current function takes the path of the file, and returns the name of players, teams, and theirs abbreviations
+    Function reads data from abbreviations.txt file, and returns a dict of racers in format
+    {abbreviation: {name: name, team: team}, ..., ...}.
     """
     with open(file_path, 'r') as unsorted_file:
         racers_data = {}
         abbreviations = unsorted_file.read().splitlines()
         abbreviations = list(filter(None, abbreviations))
         for each_racer in abbreviations:
-            separated_data = each_racer.strip().split(sep='_')
-            racers_data[separated_data[0]] = [separated_data[1], separated_data[2]]
+            abbreviation, name, team = each_racer.strip().split('_')
+            racers_data[abbreviation] = {'name': name, 'team': team}
         return racers_data
 
 
 def collecting_racers_data(start_file_path, finish_file_path, collected_data):
     """
-    Current function takes log files, add all the necessary data to previously created dictionary of lists with
-    name, and team. Then append each list with the player's start time, and finish time
+    Takes start and end paths to files, and previously created container from abbreviation file, and update the last
+    file with info from start and end file (start and finish time). If there is no info about time, func assigns None.
     """
-    players_list = file_reader(start_file_path)
+    updated_data = update_racers_info(start_file_path, collected_data, 'start_time')
+    updated_data = update_racers_info(finish_file_path, updated_data, 'finish_time')
+    for racer in updated_data:
+        if 'finish_time' not in updated_data[racer]:
+            updated_data[racer]['finish_time'] = None
+        if 'start_time' not in updated_data[racer]:
+            updated_data[racer]['start_time'] = None
+    return updated_data
+
+
+def update_racers_info(file_path, collected_data, key_title):
+    """
+    Function updates the container with info about the racer from start or end file.
+    :param file_path: path to a file (start or end file).
+    :param collected_data: previously created container with data from previously parsed files.
+    :param key_title: the name of the key we want to update the dictionary with.
+    :return: updated container.
+    """
+    players_list = file_reader(file_path)
     for players_result in players_list:
-        collected_data[players_result.strip()[:3]].append(players_result.strip()[3:])
-    players_list = file_reader(finish_file_path)
-    for players_result in players_list:
-        collected_data[players_result.strip()[:3]].append(players_result.strip()[3:])
+        racers_data = players_result.strip()
+        collected_data[racers_data[:3]].update({key_title: racers_data[3:]})
     return collected_data
 
 
 def file_reader(file_path):
-    """File reader which read the file, split it into the lines, removes empty strings, and returns the list of lines"""
+    """
+    Reads the files start and end for further processing, and returns the list of lines from a file
+    """
     with open(file_path, 'r') as file:
         players_list = file.read().splitlines()
         return list(filter(None, players_list))
@@ -89,68 +110,75 @@ def file_reader(file_path):
 
 class Racer:
     """
-    Class Racer presents each racer and takes attributes such as name, team, start time, and finish time.
-    Class has 1 method (lap_calculator) which calculates the racer's lap time
+    Class Racer represents a racer with the properties such as name, team, start_time, finish_time, lap_time, lap_time_str
     """
+
     def __init__(self, name, team, start_time, finish_time):
         self.name = name
         self.team = team
         self.start_time = start_time
         self.finish_time = finish_time
 
-    def lap_calculator(self):
-        start_time = datetime.strptime(self.start_time, '%Y-%m-%d_%H:%M:%S.%f')
-        finish_time = datetime.strptime(self.finish_time, '%Y-%m-%d_%H:%M:%S.%f')
-        return finish_time - start_time
+    @property
+    def lap_time(self):
+        if self.start_time and self.finish_time:
+            start_time = datetime.strptime(self.start_time, '%Y-%m-%d_%H:%M:%S.%f')
+            finish_time = datetime.strptime(self.finish_time, '%Y-%m-%d_%H:%M:%S.%f')
+            if finish_time - start_time < timedelta(0):
+                return None
+            else:
+                return finish_time - start_time
+        else:
+            return None
+
+    @property
+    def lap_time_str(self):
+        return str(self.lap_time)[3:-3] if self.lap_time else None
 
 
 def players_info(collected_data):
     """
-    Current function collects the list of Racer objects from the previously given data for further usage.
-    Then creates a dictionary where the key is the name, and 3 values - name, team, and lap time. From this function we
-    take data to prepare info for report, or take it for output a statistic about a particular racer
+    Func takes info about racers and returns a dict where key is the name of the racer, and value is the Racer object.
     """
-    racer_objects = [Racer(collected_data[racer][0],
-                           collected_data[racer][1],
-                           collected_data[racer][2],
-                           collected_data[racer][3]) for racer in collected_data]
-    structured_info = {}
-    for racer in racer_objects:
-        structured_info[racer.name] = {'name': racer.name,
-                                       'team': racer.team,
-                                       'lap_time': racer.lap_calculator()}
+    structured_info = {collected_data[racer]['name']:
+                           Racer(collected_data[racer]['name'],
+                                 collected_data[racer]['team'],
+                                 collected_data[racer]['start_time'],
+                                 collected_data[racer]['finish_time']) for racer in collected_data}
     return structured_info
 
 
 def build_report(structured_info, asc_flag):
     """
-    Current function takes the dictionary of previously created Racer objects, and the flag (True or False) which shows
-    in which order to sort the players. Then returns 2 lists:
-    1. The list of sorted data by time along with name and team for printing the report.
-    2. The same list, but with the racers with the incorrectly taken time.
+    Func takes dict in format {name: Racer object, ...}, and split it into 2 lists (with correct time, and incorrect).
+    Then sorts the list with correct data in requested order, and returns those 2 lists.
     """
-    info_for_report = [[key, value['team'], value['lap_time']] for key, value in structured_info.items()
-                       if value['lap_time'] >= timedelta(0)]
-    info_for_report.sort(key=lambda x: x[2], reverse=asc_flag)
-    info_with_incorrect_time = [[key, value['team'], 'Incorrect data. Check logs'] for key, value in structured_info.items()
-                                if value['lap_time'] < timedelta(0)]
+    info_for_report, info_with_incorrect_time = [], []
+    for racers_name, racer in structured_info.items():
+        if racer.lap_time:
+            info_for_report.append(racer)
+        else:
+            info_with_incorrect_time.append(racer)
+    info_for_report.sort(key=lambda x: x.lap_time, reverse=asc_flag)
     return info_for_report, info_with_incorrect_time
 
 
 def print_report(prepared_info_for_report):
-    """The function takes all ready information from build_report function, and prints all info to console"""
+    """
+    Func prints the information about all the racers with given lists with info about the racers.
+    """
     info_for_report, info_with_incorrect_time = prepared_info_for_report
     counter = 0
-    for item in info_for_report:
+    for racer in info_for_report:
         counter += 1
-        print(f"{counter} {item[0] : <20} | {item[1] : <25} | {str(item[2])[3:-3]}")
+        print(f"{counter} {racer.name : <20} | {racer.team : <25} | {racer.lap_time_str}")
         if counter == 15:
-            print(80 * '-')
-    for item in info_with_incorrect_time:
+            print(70 * '-')
+    for racer in info_with_incorrect_time:
         counter += 1
-        print(f"{counter} {item[0] : <20} | {item[1] : <25} | {item[2]}")
+        print(f"{counter} {racer.name : <20} | {racer.team : <25} | {racer.lap_time_str}")
         if counter == 15:
-            print(80 * '-')
+            print(70 * '-')
 
 
 if __name__ == '__main__':
